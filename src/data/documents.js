5a0ec55da75eca9371b7b93f8e7e49057630a64d
AkramjonDev <akramjon.ikromov0523@gmail.com>
2024-07-03 12:39:39 +0500

    last commit 5

--- src/data/documents.js
+++ src/data/documents.js
@@ -3,7 +3,13 @@ const documents = [
     name: "Guvohnoma",
     description:
       "Tadbirkorlik subyektivi davlat tomonidan ro'yxatdan o'tkazilganligi to'g'risidagi guvohnoma.",
-    url: "/documents/guvohnoma.jpg",
+    url: "/documents/923610-2c1a9c9dc3c3f72994c4a27df93885f0.pdf",
+  },
+  {
+    name: "Shartnoma",
+    description:
+      "Ushbu shartnoma «Shohimardon Ramziddin servis» M.Ch.J shartnomasi",
+    url: "/documents/Shartnoma 2024..docx",
   },
 ];
 
