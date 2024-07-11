344f5d68a7a63dfaf1bf03a67ac4f52c617cfdf8
AkramjonDev <akramjon.ikromov0523@gmail.com>
2024-07-11 12:57:47 +0500

    changed btn title

--- src/components/PricingPlans.js
+++ src/components/PricingPlans.js
@@ -38,7 +38,7 @@ const PricingTable = () => {
               onClick={toggleAccordion}
               className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-200"
             >
-              Hujjatlarni ko'rish
+              Narxlar bo'yicha hujjatlarni ko'rish
             </button>
             {isOpen && (
               <div className="mt-4">
